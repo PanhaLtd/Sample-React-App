1bddeebc834ba0b3c26ac95d04e0321b32b7c94e
PanhaLtd <panhaltb@gmail.com>
2020-12-12 00:50:15 +0700

    Sample React App

---
+++ client/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="content has-text-centered">
+        <h1>
+           Musician App 201.
+        </h1>
+      </div>
+    </footer>
+  )
+}
